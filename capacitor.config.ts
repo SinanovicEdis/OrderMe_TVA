85df328d41275ee16d40a4f1079e0916214119fd
edis <edis.sinanovi5@gmail.com>
2024-05-13 13:51:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OrderMe_TVA',
+  webDir: 'build'
+};
+
+export default config;
